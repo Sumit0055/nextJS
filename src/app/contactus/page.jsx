d48ac673e15764c32fcd395c51971e43bfc0b18f
Sumit0055 <sumitduttsumitdutt@gmail.com>
2025-12-27 16:18:27 +0530

    Query Parameter Client and Server Components

--- src/app/contactus/page.jsx
+++ src/app/contactus/page.jsx
@@ -1,7 +1,33 @@
+//---Query Parameters in Client Component
+// "use client"
+// import { useSearchParams } from 'next/navigation'
+// import React from 'react'
+
+// export default function contactus() {
+//   let searchParams = useSearchParams()
+//   return (
+//     <>
+//     <h1>This is Contactus Page</h1>
+//     {searchParams.get("name")?<h2>Name : {searchParams.get("name")}</h2>:null}
+//     {searchParams.get("dsg")?<h2>Designation : {searchParams.get("dsg")}</h2>:null}
+//     {searchParams.get("salary")?<h2>Salary : {searchParams.get("salary")}</h2>:null}
+//     </>
+//   )
+// }
+
+//---Query Parameters in Servcer Component
+
 import React from 'react'
 
-export default function contactus() {
+export default async function contactus(req, res) {
+  let { name, dsg, salary } = await req.searchParams
+  console.log(req.searchParams)
   return (
-    <div> COntact usPage</div>
+    <>
+      <h1>This is Contactus Page</h1>
+      {name ? <h2>Name : {name}</h2> : null}
+      {dsg ? <h2>Designation : {dsg}</h2> : null}
+      {salary ? <h2>Salary : {salary}</h2> : null}
+    </>
   )
 }
